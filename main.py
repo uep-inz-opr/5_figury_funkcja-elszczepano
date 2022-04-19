863f12a3f5ffc9b0102bfbc70243f76c4e915aa3
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2022-04-19 12:51:37 +0200

    Refactor

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ figures = []
 # Collect documents
 for index in range(0, i):
     dimensions = input().split( " " )
-
-    figures.append(map(int, dimensions))
-
+    figures.append(dimensions)
 
 def calculate():
     sum = 0;
 
     for figure in figures:
-        if( len(figure) == 1 ):
-            sum += circle( figure[ 0 ] )
+        figureDimensions = list(map(int, figure))
+        
+        if( len(figureDimensions) == 1 ):
+            sum += circle( figureDimensions[ 0 ] )
 
             continue;
-        if( len(figure) == 2 ):
-            sum += rectangle( figure[ 0 ], figure[ 1 ] )
+        if( len(figureDimensions) == 2 ):
+            sum += rectangle( figureDimensions[ 0 ], figureDimensions[ 1 ] )
             
             continue;
-        if( len(figure) == 3 ):
-            sum += triangle( figure[ 0 ], figure[ 1 ], figure[ 2 ] )
+        if( len(figureDimensions) == 3 ):
+            sum += triangle( figureDimensions[ 0 ], figureDimensions[ 1 ], figureDimensions[ 2 ] )
             
             continue;
-    print( "Błąd: można podać maksymalnie 3 liczby" )
+        print( "Błąd: można podać maksymalnie 3 liczby" )
 
-    exit(0)
+        exit(0)
 
 def rectangle(width, height):
     return width * height
@@ -38,7 +38,7 @@ def circle(radius):
     return math.pi * radius * radius
 
 def triangle(a,b,c):
-    s = (a+b+c)/2
-    return math.sqrt(s(s-a)*(s-b)*(s-c))
+    s = int((a+b+c)/2)
+    return math.sqrt(s*(s-a)*(s-b)*(s-c))
 
 calculate()
