125b1c8b0871f3d3ab51c9a741190e18b82daf23
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2022-04-19 12:36:23 +0200

    Prototype

---
+++ main.py
@@ -0,0 +1,44 @@
+import math
+
+i = int( input() )
+
+figures = []
+
+# Collect documents
+for index in range(0, int):
+    dimensions = input().split( " " )
+
+    figures.append(map(int, dimensions))
+
+
+def calculate():
+    sum = 0;
+
+    for figure in figures:
+        if( figure.size() == 1 ):
+            sum += circle( figure[ 0 ] )
+
+            continue;
+        if( figure.size() == 2 ):
+            sum += rectangle( figure[ 0 ], figure[ 1 ] )
+            
+            continue;
+        if( figure.size() == 3 ):
+            sum += triangle( figure[ 0 ], figure[ 1 ], figure[ 2 ] )
+            
+            continue;
+    print( "Błąd: można podać maksymalnie 3 liczby" )
+
+    exit(0)
+
+def rectangle(width, height):
+    return width * height
+
+def circle(radius):
+    return math.pi * radius * radius
+
+def triangle(a,b,c):
+    s = (a+b+c)/2
+    return math.sqrt(s(s-a)*(s-b)*(s-c))
+
+calculate()
